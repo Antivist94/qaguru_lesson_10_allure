5c82524188c76ae77a0ee5872ee3ee37ab7db57e
Eugene Evdokimenko <evdokimenko@tochka.com>
2024-07-09 01:01:42 +0400

    upd conftest

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 def browser_manager():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_height = '1080'
-    browser.config.window_width = '1220'
+    browser.config.window_width = '1920'
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
